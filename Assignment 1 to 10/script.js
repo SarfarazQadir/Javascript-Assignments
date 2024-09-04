4f575f6bc6d62d13e50f8889a2b431b30d628833
Sarfaraz Qadir <sarfarazqadir594@gmail.com.com>
2024-09-04 19:25:53 +0500

    first commit

---
+++ Assignment 1 to 10/script.js
@@ -0,0 +1,255 @@
+// Assignment 1
+
+// alert("Error! Please enter a valid password.");
+
+// alert(" Welcome to JS Land... \n Happy Coding");
+// alert(` Happy Coding \n <input type="checkbox">Prevent this page from creating additional dialogs.`);
+// alert(" Hello... I can run JS through my browser's console");
+
+
+// Assignment 2
+
+// var username ;
+
+// var myName = "Sarfaraz";
+// var myAge = 18;
+// var myEmail = "sarfarazqadir594@gmail.com";
+// var Book = "A smarter way to learn JavaScript";
+
+
+
+// alert("My name is"+myName);
+// alert("My age is"+myAge);
+// alert("My email is"+myEmail);
+// alert("I am trying to learn The book "+Book);
+
+// alert(" PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// document.write("Yeah I can write HTML through JS");
+
+// var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+// alert(symbol);
+
+
+// Assignment 3
+
+// var myAge = 18;
+
+// alert("My age is"+myAge);
+
+// var birthYear = 2002;
+
+// alert("My Birth YEar is "+birthYear);
+
+// var store = "XYZ";
+// var visitorname = "Sarfaraz";
+// var title = " T Shirt"
+// var quantity = 5;
+
+// alert(visitorname + " ordered " + quantity + title + " on " + store + " clothing");
+
+        //  Assignment No 4 
+
+        // var $ = "$";
+        // var _ = "_";
+        // var $my_1stVariable = "$my_1stVariable";
+
+        
+       
+        // document.write("<h1>Rules for naming JS variables </h1>"); 
+       
+        // document.write("Variable names can only only contain, numbers "+$ + " and "+_+". For example : "+"$my_1stVariable <br> Variable must be begin with a letter, "+$+" or "+_+". For example : $name, _name or name <br> Variable names are case sensitive <br> Variable names should not be JS keyword.")
+
+//  Assignment No 5 
+
+//  Addition
+
+// var num1 = 3;
+// var num2 = 5;
+
+// var result = document.write("Sum of "+num1+" and "+num2+" is "+ num1+num2);
+
+//  Subtraction
+
+// var num1 = 3;
+// var num2 = 5;
+
+// var result = num1-num2;
+
+//  document.write("Subtraction of 3 and 5 is "+ result)
+        
+ //  Multiplication
+
+//  var num1 = 3;
+//  var num2 = 5;
+//  var result = num1*num2;
+//  document.write("Multiplication of 3 and 5 is "+ result)
+
+ //  Division
+
+ //  var num1 = 3;
+//  var num2 = 5;
+//  var result = num1/num2;
+//  document.write("Division of 3 and 5 is "+ result)
+
+ // Modulus
+
+//  var num1 = 3;
+//  var num2 = 5;
+//  var result = num1%num2;
+//  document.write("Modulus of 3 and 5 is "+ result)
+
+
+// var var1 = 5;
+
+// document.write("Value after variable declaration is undefined<br>")
+// document.write("Intial value: "+var1);
+
+// var var2 = var1++;
+
+// document.write("<br>Value after increament is: "+var1);
+
+// var result = var1 + 7;
+
+// document.write("<br>Value after addition is: "+result)
+
+// var var3 = result--;
+
+// document.write("<br>Value after increament is: "+result);
+
+// var result2 = result%3;
+// document.write("<br>Value after division is: "+result2);
+
+
+// var ticket = 600*5;
+
+// document.write("Total cost to buy 5 tickets to a movie " +ticket+" PKR");
+
+
+// var num1 = 4*1;
+// document.write("4 x 1 = "+num1);
+// var num2 = 4*2;
+// document.write("<br>4 x 2 = "+num2);
+// var num3 = 4*3;
+// document.write("<br>4 x 3 = "+num3);
+// var num4 = 4*4;
+// document.write("<br>4 x 4 = "+num4);
+// var num5 = 4*5;
+// document.write("<br>4 x 5 = "+num5);
+// var num6 = 4*6;
+// document.write("<br>4 x 6 = "+num6);
+// var num7 = 4*7;
+// document.write("<br>4 x 7 = "+num7);
+// var num8 = 4*8;
+// document.write("<br>4 x 8 = "+num8);
+// var num9 = 4*9;
+// document.write("<br>4 x 9 = "+num9);
+// var num10 = 4*10;
+// document.write("<br>4 x 10 = "+num10);
+
+
+// var celsius = +prompt("Enter Temprature");
+
+
+// var fahrenheit = (celsius - 32) * 5/9;
+//  document.write("Temperature in fahrenheit is: "+fahrenheit);
+
+
+// var fahrenheit = +prompt("Enter Temprature");
+
+// var celsius = (fahrenheit * 9/5) + 32;
+
+// document.write("Temperature in celsius is: "+celsius);
+
+// document.write("<h1>Shopping Cart</h1>")
+
+// var item1 = +prompt("Enter price of item 1");
+// var item2 = +prompt("Enter price of item 2");
+// var quantity1 = +prompt("Enter the quantity of item 1");
+// var quantity2 = +prompt("Enter the quantity of item 2");
+// var shippingcharges = 200;
+
+// var total1 = item1*quantity1;
+// var total2 = item2*quantity2;
+// var total = total1 + total2 + shippingcharges;
+// document.write("Price of Item 1 is "+item1);
+// document.write("<br>Quantity of Item 1 is "+quantity1);
+// document.write("<br>Price of Item 2 is "+item2);
+// document.write("<br>Quantity of Item 2 is "+quantity2);
+// document.write("<br>Shipping Charges are "+shippingcharges);
+// document.write("<br>Total cost of your order is "+total);
+
+
+// document.write("<h1>Mark Sheet</h1>")
+
+// var totalmarks = 300;
+// var obtainedmarks = +prompt("Enter the marks you obtained ");
+// var percentage = (obtainedmarks/totalmarks)*100;
+// document.write("Total Marks: "+totalmarks);
+// document.write("<br>Obtained Marks: "+obtainedmarks);
+// document.write("<br>Percentage: "+percentage+"%");
+
+// document.write("<h1>Currency Convertor</h1>")
+
+// var doller = 250;
+// var riyal = 25;
+
+// var amount = +prompt("Enter the amount you want to convert");
+// var currency = prompt("Enter the currency you want to convert to");
+// if (currency == "Doller") {
+//   var convertedamount = amount * doller;
+//   document.write("Total currency in PKR is: " + convertedamount);
+// } else if (currency == "Riyal") {
+//   var convertedamount = amount * riyal;
+//   document.write("Total currency in PKR is: " + convertedamount);
+// } else {
+//   document.write("Invalid currency");
+// }
+
+
+// var num = 10;
+
+// num = num+5;
+// num = num*10;
+// num = num/2;
+// document.write(num)
+
+
+// document.write("<h1>Age Calculator</h1>")
+
+// var user = +prompt("Enter your birth year");
+
+// var age = 2024 - user;
+
+// document.write("Birth Year "+user)
+// document.write("<br>Your age is "+age)
+
+
+// document.write("<h1>The Geometrized Circle</h1>")
+
+// var radius = 3;
+
+// var circumference = 2 * 3.14 * radius;
+// var area = 3.14 * radius * radius;
+
+
+// document.write("Area of radius is "+radius)
+// document.write("<br>The Circumference "+circumference)
+// document.write("<br>The Area is "+area)
+
+
+
+// document.write("<h1>Lifetime Calculator</h1>")
+
+// var snack = "Slims";
+// var age = 20;
+// var maxage = 45;
+// var amountsnack = 2;
+
+// var total = (maxage-age)*amountsnack*365;
+
+// document.write("You'll need "+total+" " +snack+" to last you until the ripe old age of "+maxage)
+
+
+//  Assignment No. 6
